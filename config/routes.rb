72f0eda6ccccdb4693671a70b2c66e80fe6f6b89
paulhenke <paulmhenke@gmail.com>
2015-06-28 18:10:44 +0000

    Added users#show and path to edit user info

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   root to: 'home#index'
   
   devise_for :users
+  resources :users, only: [:show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
