7aaa99c773474db2abb1809a832383bb0a83837f
paulhenke <paulmhenke@gmail.com>
2015-06-29 19:37:17 +0000

    mailgun integration for incoming emails

---
+++ app/controllers/incoming_controller.rb
@@ -0,0 +1,28 @@
+class IncomingController < ApplicationController
+
+  # http://stackoverflow.com/questions/1177863/how-do-i-ignore-the-authenticity-token-for-specific-actions-in-rails
+  skip_before_action :verify_authenticity_token, only: [:create]
+
+  def create
+    # You put the message-splitting and business
+    # magic here.
+    # Find the user by using params[:sender]
+    @user = User.find_by(email:params[:sender])
+    # Find the topic by using params[:subject]
+    @topic = Topic.find_by(title:params[:subject])
+    # Assign the url to a variable after retreiving it from params["body-plain"]
+    @url = "#{params["body-plain"]}"
+
+    # Check if user is nil, if so, create and save a new user
+    @user = User.create!(email: params[:sender]) if @user.nil?
+
+    # Check if the topic is nil, if so, create and save a new topic
+    @topic = Topic.create!(title: params[:title], user_id: @user.id) if @topic.nil?
+
+    # Now that you're sure you have a valid user and topic, build and save a new bookmark
+    @bookmark = Bookmark.create!(url: @url, topic_id: @topic.id)
+
+    # Assuming all went well.
+    head 200
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
+    @topics = @user.topics
   end
 end
