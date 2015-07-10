0aae17140e06d13eab430c1215bf738f3b1ef397
paulhenke <paulmhenke@gmail.com>
2015-07-10 15:40:39 +0000

    bookmark and topic controllers first version

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -8,17 +8,39 @@ class BookmarksController < ApplicationController
   end
   
   def create
-    
-    
+    @topic = Topic.find(params[:topic_id])
+    @bookmark = current_user.bookmarks.build(params.require(:boomark).permit(:url))
+    @bookmark.topic = @topic
+    if @bookmark.save
+      flash[:notice] = "Your bookmark was saved."
+      redirect_to @topic
+    else
+      redirect_to root
+    end
   end
 
   def edit
+    @bookmark = Bookmark.find(params[:id])
   end
   
   def update
+    @bookmark = Bookmark.find(params[:id])
+    if @bookmark.update_attributes(params.require(:bookmark).permit(:url))
+      flash[:notice] = "Updated successfully."
+      redirect_to @topic
+    else
+      redirect_to root
+    end
   end
   
   def destroy
+    @bookmark = Bookmark.find(params[:id])
+    if @bookmark.destroy
+      flash[:notice] = "Bookmark has been deleted."
+      redirect_to @topic
+    else
+      redirect_to root
+    end
   end
   
 end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -14,11 +14,41 @@ class TopicsController < ApplicationController
   end
   
   def create
+    @topic = Topic.new(topic_params) #? should I just think of this as the standard way of doing this? 
+    if @topic.save 
+      flash[:notice] = "Topic created succesfully." 
+    else
+      redirect_to root
+    end
   end
 
   def edit
+    @topic = Topic.find(params[:id])
   end
   
   def update
+    @topic = Topic.find(params[:id])
+    if @topic.update_attributes(topic_params)
+      redirect_to @topic
+    else
+      redirect_to root
+    end
   end
+  
+  def destroy
+    @topic = Topic.find(params[:id])
+    if @topic.destroy
+      flash[:notice] = "Topic was deleted."
+      redirect_to topics_path
+    else
+      redirect_to root
+    end
+  end
+  
+  private
+  
+  def topic_params
+    params.require(:topic).permit(:title)
+  end
+  
 end
