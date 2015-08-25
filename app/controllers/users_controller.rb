2e9d4ce1346dcde783718a3e183269e93636da11
paulhenke <paulmhenke@gmail.com>
2015-08-25 19:05:18 +0000

    fixed destroying bookmarks with ajax

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
   def show
+    
     @user = current_user
     @user_bookmarks = Bookmark.where(creator_id: user.id)
     @liked_bookmarks = @user.liked_bookmarks
+    
     
   end
 end
