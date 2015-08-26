b0d75f3811d16bf6d9080769f2c86861c6879b89
paulhenke <paulmhenke@gmail.com>
2015-08-26 23:45:01 +0000

    finished users#show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,10 @@ class UsersController < ApplicationController
   def show
     
     @user = current_user
-    @user_bookmarks = Bookmark.where(creator_id: user.id)
+    @user_bookmarks = Bookmark.where(creator_id: @user.id) #? even though I don't use these in views, acceptable to put them here
     @liked_bookmarks = @user.liked_bookmarks
-    
+    @user_bookmark_topics = @user_bookmarks.group_by { |ubm| ubm.topic }
+    @liked_bookmark_topics = @liked_bookmarks.group_by { |lbm| lbm.topic }
     
   end
 end
